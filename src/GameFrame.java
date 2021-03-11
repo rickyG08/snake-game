b45f81dccb04b036077d94ca0003afb6bc4fea16
Ricky Garcia <rickyg08@msn.com>
2021-03-11 12:11:18 -0700

    Not running. But should be ready to go once fixed.

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -3,7 +3,6 @@ import javax.swing.JFrame;
 public class GameFrame extends JFrame {
 
   GameFrame(){
-
     this.add(new GamePanel());
     this.setTitle("Snake");
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -12,5 +11,4 @@ public class GameFrame extends JFrame {
     this.setVisible(true);
     this.setLocationRelativeTo(null);
   }
-
 }
